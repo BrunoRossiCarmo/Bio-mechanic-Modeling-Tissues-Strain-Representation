18b71b6192a02dd9d0b3f1c3f9f50a9e72a99e31
Pagodeiro9898 <71408712+Pagodeiro9898@users.noreply.github.com>
2020-09-17 16:34:28 -0300

    Add files via upload

--- filipe/teste2.py
+++ filipe/teste2.py
@@ -10,6 +10,7 @@ from vpython import *
 import matplotlib.pyplot as plt #import matplotlib library
 from drawnow import *
 import time
+from math import*
 
 
 m = 0.01 
@@ -18,9 +19,9 @@ H = 0.02
 W = 0.02 
 x = 0.1
 R = 0
-E= 10000
 Def = 0
 Tensao = 0
+
 
 d = 0.1
 LG = 0.2
@@ -32,7 +33,6 @@ cylinderO = cylinder(pos = vector(-(LG/2)+x, H-0.01, W), size = vector(1.5*L, 1.
 #arrow1 = arrow(pos = vector(-(LG/3)+x, H+0.01, W), size = vector(L, H, W), color = color.red);
 
 
-EBox = label(pos = vec(-0.06, 0.06, 0), text = 'E = %d Pa' %(E))
 
 tenPA = []
 Deform = []
@@ -47,23 +47,25 @@ def grafico():
 
 for cont in range(0, 20):
     rate(3)
+    E = 10000
     R = R + 0.0007
-    #print(R)
     cylinderO.pos = vector(-(LG/2)+x, H-0.01-R, W)
     block.size = vector(L+R, (H - R), W+2*(R))
     block.pos = vector(-(LG/2)+x, -R/2, W)
     Def = ((L-(L+R))/L,(H-(H - R))/H, (W-(W+2*(R)))/W)
     Defy = (H-(H - R))/H
-    #print(Defy)
-    from math import expm1
-    Tensao = E*Defy*expm1(-5*E)
+    Tensao = E*Defy*exp(-5*E)
     print(Tensao)
     EBox = label(pos = vec(0, 0.06, 0), text = 'Deformacao = %1.2f' %(Defy))
     EBox = label(pos = vec(0.06, 0.06, 0), text = 'Tensao = %1.2f N' %(Tensao))
+    EBox = label(pos = vec(-0.06, 0.06, 0), text = 'E = %d Pa' %(E))
     tenPA.append(Tensao)
     Deform.append(Defy)
     drawnow(grafico)
     time.sleep(0.005)
+    
+
+
 
 
     
